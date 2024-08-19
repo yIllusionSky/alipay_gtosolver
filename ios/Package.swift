5ff888055329a43c7caa62df94dd2bd776fc8565
YaoLinQing <d161442079@163.com>
2024-08-19 15:51:13 +0800

    push

--- ios/Package.swift
+++ ios/Package.swift
@@ -8,7 +8,7 @@ import PackageDescription
 let package = Package(
     name: "tauri-plugin-clipboard-manager",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v14),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
